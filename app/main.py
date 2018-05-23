6d5ca6f19dbb5de1bc95b6bdc17cd0c532a38633
Kien Nguyen <kiennt23@gmail.com>
2018-05-24 01:09:17 +0800

    To calculate ROI for back testing

--- app/main.py
+++ app/main.py
@@ -22,9 +22,9 @@ class DCEventType(set):
 
 
 class Position:
-    def __init__(self):
-        self.qty = 0
-        self.price = 0
+    def __init__(self, qty, price):
+        self.qty = qty
+        self.price = price
         self.status = 'OPEN'
 
 
@@ -37,26 +37,28 @@ client = pymongo.MongoClient("mongodb+srv://bat-price-watcher:QRTHQ3MfX5ia0oMh@c
 db = client['bat-price-watcher']
 
 
-# start aggregated trade websocket for BNBBTC
 def process_message(event):
     global mode
     global LAMBDA
     global p_ext
+    global position
     p_t = float(event['k']['c'])
     # event_time = datetime.fromtimestamp(event['E']/1000)
     # event_time = event_time.replace(tzinfo=sing_tz)
-    event['_id'] = event['E']
-    try:
-        symbol_collection = db[SYMBOL]
-        symbol_collection.insert_one(event)
-    except DuplicateKeyError:
-        logger.error('Duplicate event {}'.format(event['_id']))
+    # event['_id'] = event['E']
+    # try:
+    #     symbol_collection = db[SYMBOL]
+    #     symbol_collection.insert_one(event)
+    # except DuplicateKeyError:
+    #     logger.error('Duplicate event {}'.format(event['_id']))
     if mode == event_type.UPTURN:
         if p_t <= p_ext * (1.0 - LAMBDA):
             logger.info('p_ext={} p_t={}'.format(p_ext, p_t))
             mode = event_type.DOWNTURN
             p_ext = p_t
             logger.info('BUY TF mode={} p_t={}'.format(str(mode), p_t))
+            # When BUY, create a new Position
+            position = Position(1.0, p_t)
         else:
             p_ext = max([p_ext, p_t])
             logger.info('p_ext={} p_t={}'.format(p_ext, p_t))
@@ -67,6 +69,9 @@ def process_message(event):
             mode = event_type.UPTURN
             p_ext = p_t
             logger.info('SELL TF mode={} p_t={}'.format(str(mode), p_t))
+            # When SELL, close position
+            roi = (position.price - p_t) / position.price
+            logger.info('ROI {}'.format(str(roi)))
         else:
             p_ext = min([p_ext, p_t])
             logger.info('p_ext={} p_t={}'.format(p_ext, p_t))
