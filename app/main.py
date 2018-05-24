c2ab4c191f62c777048573b0bdabe409489864a3
Kien Nguyen <kiennt23@gmail.com>
2018-05-25 01:50:10 +0800

    Add actual buy and sell order

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import logging
 import pymongo
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
 
@@ -22,7 +22,8 @@ class DCEventType(set):
 
 
 class Position:
-    def __init__(self, qty, price):
+    def __init__(self, order_id, qty, price):
+        self.order_id = order_id
         self.qty = qty
         self.price = price
         self.status = 'OPEN'
@@ -30,14 +31,36 @@ class Position:
 
 event_type = DCEventType(['UPTURN', 'DOWNTURN'])
 mode = event_type.DOWNTURN
-LAMBDA = 0.004
-p_ext = 0.0
+p_ext = INITIAL_P_EXT
 sing_tz = tz.gettz('UTC+8')
-client = pymongo.MongoClient("mongodb+srv://bat-price-watcher:QRTHQ3MfX5ia0oMh@cluster0-w2mrr.mongodb.net/bat-price-watcher?retryWrites=true")
-db = client['bat-price-watcher']
+mongo_client = pymongo.MongoClient("mongodb+srv://bat-price-watcher:QRTHQ3MfX5ia0oMh@cluster0-w2mrr.mongodb.net/bat-price-watcher?retryWrites=true")
+db = mongo_client['bat-price-watcher']
+
+client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
+exchange_info = client.get_exchange_info()
+symbol_info = [symbol for symbol in exchange_info['symbols'] if symbol['symbol'] == SYMBOL].pop()
+base_asset = symbol_info['baseAsset']
+quote_asset = symbol_info['quoteAsset']
+quote_asset_precision = symbol_info['quotePrecision']
+lot_size_filter = [symbol_filter for symbol_filter in symbol_info['filters'] if 'LOT_SIZE' == symbol_filter['filterType']].pop()
+minQty = lot_size_filter['minQty'].rstrip('0')
+dot_index = minQty.find('.')
+if -1 != dot_index:
+    minQty = minQty[minQty.find('.') + 1:]
+
+base_asset_precision = len(minQty)
+
+maxQty = lot_size_filter['maxQty']
+
+base_asset_balance = client.get_asset_balance(asset=base_asset)
+logger.debug(base_asset_balance)
+quote_asset_balance = client.get_asset_balance(asset=quote_asset)
+logger.debug(quote_asset_balance)
+
+bm = BinanceSocketManager(client)
 
 
-def process_message(event):
+def process_kline(event):
     global mode
     global LAMBDA
     global p_ext
@@ -53,39 +76,70 @@ def process_message(event):
         logger.error('Duplicate event {}'.format(price_doc['_id']))
     if mode == event_type.UPTURN:
         if p_t <= p_ext * (1.0 - LAMBDA):
-            logger.info('p_ext={} p_t={}'.format(p_ext, p_t))
+            logger.debug('p_ext={} p_t={}'.format(p_ext, p_t))
             mode = event_type.DOWNTURN
             p_ext = p_t
             logger.info('SELL CT mode={} p_t={}'.format(str(mode), p_t))
             # When SELL, close position
-            if position is not None:
-                roi = (position.price - p_t) / position.price
-                logger.info('ROI {}'.format(str(roi)))
+            if position is not None and 'FILLED' == position.status:
+                quote_qty = str(round(position.qty, quote_asset_precision))
+                order_response = client.order_limit_sell(symbol=SYMBOL, quantity=quote_qty, price=p_t)
+                logger.debug('ORDER {}'.format(order_response))
+                roi = ((position.price - p_t) / position.price) - (2 * COMMISSION_RATE)
+                if ORDER_STATUS_FILLED == order_response['status']:
+                    logger.info('ROI {}'.format(str(roi)))
+                else:
+                    logger.info('Estimated ROI {}'.format(str(roi)))
         else:
             p_ext = max([p_ext, p_t])
-            logger.info('p_ext={} p_t={}'.format(p_ext, p_t))
+            logger.debug('p_ext={} p_t={}'.format(p_ext, p_t))
 
     else:  # mode is DOWNTURN
         if p_t >= p_ext * (1.0 + LAMBDA):
-            logger.info('p_ext={} p_t={}'.format(p_ext, p_t))
+            logger.debug('p_ext={} p_t={}'.format(p_ext, p_t))
             mode = event_type.UPTURN
             p_ext = p_t
             logger.info('BUY CT mode={} p_t={}'.format(str(mode), p_t))
+            free_quote_balance = float(quote_asset_balance['free'])
+            base_by_quote_balance = free_quote_balance / p_t
+            base_qty = str(round(base_by_quote_balance, base_asset_precision))
+            logger.debug(base_qty)
+            order_response = client.order_limit_buy(symbol=SYMBOL, quantity=base_qty, price=p_t)
+            logger.debug('ORDER {}'.format(order_response))
             # When BUY, create a new Position
-            position = Position(1.0, p_t)
+            if ORDER_STATUS_FILLED == order_response['status']:
+                qty = float(order_response['executedQty'])
+                position = Position(order_response['orderId'], qty, p_t)
+                position.status = 'FILLED'
+            else:
+                position = Position(order_response['orderId'], 0.0, p_t)
         else:
             p_ext = min([p_ext, p_t])
-            logger.info('p_ext={} p_t={}'.format(p_ext, p_t))
+            logger.debug('p_ext={} p_t={}'.format(p_ext, p_t))
+
+
+def process_user_data(event):
+    if 'outboundAccountInfo' == event['e']:
+        base_asset_event = [asset for asset in event['B'] if asset['a'] == base_asset].pop()
+        base_asset_balance['free'] = base_asset_event['f']
+        base_asset_balance['locked'] = base_asset_event['l']
+        logger.debug(base_asset_balance)
+        quote_asset_event = [asset for asset in event['B'] if asset['a'] == quote_asset].pop()
+        quote_asset_balance['free'] = quote_asset_event['f']
+        quote_asset_balance['locked'] = quote_asset_event['l']
+        logger.debug(quote_asset_balance)
+    if 'executionReport' == event['e']:
+        order_id = event['i']
+        order_status = event['X']
+        if order_id == position.order_id and 'OPEN' == position.status and ORDER_STATUS_FILLED == order_status:
+            position.qty += float(event['l'])
+            position.status = 'FILLED'
 
 
 def main():
     logger.info('Starting crypto watch for {}'.format(SYMBOL))
-    client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
-    exchange_info = client.get_exchange_info()
-    symbol_info = [symbol for symbol in exchange_info['symbols'] if symbol['symbol'] == 'BNBBTC'].pop()
-
-    bm = BinanceSocketManager(client)
-    bm.start_kline_socket(SYMBOL, process_message, interval=KLINE_INTERVAL_1MINUTE)
+    bm.start_kline_socket(SYMBOL, process_kline, interval=KLINE_INTERVAL_1MINUTE)
+    bm.start_user_socket(process_user_data)
     bm.start()
 
 
